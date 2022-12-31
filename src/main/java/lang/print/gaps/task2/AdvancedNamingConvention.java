8a3a77fce035d18d62d82b5025e71d652b8d7a25
w3schools-test <test@w3school.com>
2022-12-31 12:29:35 +0500

    Ready

---
+++ src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
@@ -0,0 +1,17 @@
+package lang.print.gaps.task2;
+
+public class AdvancedNamingConvention {
+    private static final int adultAge = 18;
+
+    private int age;
+    private int phoneNumber;
+
+    void callToFriend() {
+        //some code
+    }
+
+
+    void callByNumber(int number) {
+        //some code
+    }
+}
